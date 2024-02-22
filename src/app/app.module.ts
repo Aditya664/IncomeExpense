99916d8cbdb5ba25c6a69f7a92a336e0891bce13
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2024-02-22 23:23:22 +0530

    created income expense project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ClarityModule } from "@clr/angular";
+import '@clr/icons';
+import '@cds/core/icon/register.js';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ClarityModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
